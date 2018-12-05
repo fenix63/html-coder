b9accd93847f12d695e4ad148f1e6c71a3e41667
Youri Maratkanov <Maratkanov.IUS@nsd.ru>
2018-12-05 15:42:26 +0300

    Ещё правки

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// $(document).ready(function(){
+// 	$('body').bind('mousewheel', function(e){
+// 		var destination = $('.block_2').offset().top;
+// 		console.log(destination);
+// 		$('body').animate({scrollTop: destination}, 1100);
+// 	});
+// });
